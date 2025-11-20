b60cc86390c50669829c3b14b14a44e482f5a6ea
Egor <lotus9200@gmail.com>
2025-11-20 18:58:25 +0600

    second commit

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+import time
+import pandas as pd
+
 from helpers.file_operations import get_file_data, get_directory_files
+from helpers.table_exporter import export_table
 
 
 def main():
@@ -8,6 +12,9 @@ def main():
     print(f"Referense data loaded. len: {len(referense_data)}")
     print(f"Loaded {len(input_files)} input files.\n{'\n'.join([code for code, _ in input_files])}")
 
+    start = time.time()
+    export_table(input_files[0][0], input_files[0][1])
+    print(f"Processing time: {time.time() - start} seconds")
 
 if __name__ == "__main__":
     main()
